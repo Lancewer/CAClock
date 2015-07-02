25ae6c4b254936a637cf6c6d416b85cae64208d5
lancewer <lancewer@gmail.com>
2015-07-02 14:31:13 +0800

    finish clock demo

--- CAClock/LAWViewController.m
+++ CAClock/LAWViewController.m
@@ -9,6 +9,11 @@
 #import "LAWViewController.h"
 
 @interface LAWViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *hourHand;
+@property (weak, nonatomic) IBOutlet UIImageView *minuteHand;
+@property (weak, nonatomic) IBOutlet UIImageView *secondHand;
+@property (weak, nonatomic) IBOutlet UIImageView *clockFace;
+@property (nonatomic, weak) NSTimer *timer;
 
 @end
 
@@ -16,7 +21,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //Adjust anchor points
+    self.secondHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    self.minuteHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    self.hourHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    UIScreen *mainScreen = [UIScreen mainScreen];
+    CGPoint center = CGPointMake(mainScreen.bounds.size.width / 2.0f, mainScreen.bounds.size.height / 2.0f);
+    self.secondHand.center = center;
+    self.minuteHand.center = center;
+    self.hourHand.center = center;
+    self.clockFace.center = center;
+    
+    //Timer
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
+    [self tick];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +43,20 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)tick{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSUInteger units = kCFCalendarUnitHour | kCFCalendarUnitMinute | kCFCalendarUnitSecond;
+    NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
+    //2pi = 360 degree
+    CGFloat hoursAngle = (components.hour / 12.0) * M_PI * 2.0;
+    CGFloat minutesAngle = (components.minute / 60.0) * M_PI * 2.0;
+    CGFloat secondsAngle = (components.second / 60.0) * M_PI * 2.0;
+    
+    //rotate hands
+    self.hourHand.transform = CGAffineTransformMakeRotation(hoursAngle);
+    self.minuteHand.transform = CGAffineTransformMakeRotation(minutesAngle);
+    self.secondHand.transform = CGAffineTransformMakeRotation(secondsAngle);
 }
-*/
 
 @end
