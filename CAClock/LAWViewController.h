82f0ee0db5d7a61667ee2251a69a69fbab14c828
lancewer <lancewer@gmail.com>
2015-07-02 11:31:14 +0800

    init commit and add image sets

--- CAClock/LAWViewController.h
+++ CAClock/LAWViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LAWViewController.h
 //  CAClock
 //
 //  Created by Lawrence on 7/2/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LAWViewController : UIViewController
 
 @end
-
---
+++ CAClock/LAWViewController.m
@@ -0,0 +1,37 @@
+//
+//  LAWViewController.m
+//  CAClock
+//
+//  Created by Lawrence on 7/2/15.
+//  Copyright (c) 2015 lancewer. All rights reserved.
+//
+
+#import "LAWViewController.h"
+
+@interface LAWViewController ()
+
+@end
+
+@implementation LAWViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
